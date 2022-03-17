7b749f5978ae9259a07d453347e183e9e95ba3a4
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-17 15:48:09 +0000

    2° snecchino

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
 // SECTION - Snecchino 1 - Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
-
-var array1Sneck1 = []
-var x = parseInt (0)
-var y = parseInt (0)
-
-// while (x => 50) {
-//   let z = prompt ("inserisci un numero")
-//   array1Sneck1.push(z)
-//   x += z
-// }
-
-// for ( i = 0; x <= 50; i++) {
-//   let z = prompt ("inserisci un numero");
-//   array1Sneck1.push(z);
-//   x += z;}
-
+alert("Primo snecchino")
+var array1Sneck1 = [];
+var x = parseInt (0);
+var y = parseInt (0);
+var z = parseInt (0);
 do {
   let z = parseInt (prompt ("inserisci un numero"));
   x = x + z;
@@ -24,3 +13,14 @@ do {
 document.getElementById("snak1Length").innerHTML = `Hai generato ${array1Sneck1.length} numeri, di cui la somma è ${x} Indi hai superato la tua quota di 50 e il giochino è finito. Buona giornata.`
 // !SECTION - Snecchino 1 fine
 
+// SECTION -Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
+alert("Secondo snecchino")
+
+
+y =  parseInt (prompt ("inserisci uil numero N numero"));
+
+do {
+  z = z+1;
+  console.log(y)
+  console.log(z)
+} while (z < y);
