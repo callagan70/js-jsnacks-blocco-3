0d1cc4452f5ac2acb6cee3fefd759fd3c31f335c
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-17 20:03:33 +0000

    Snecchino 3 digerito, passo al bonus

--- main.js
+++ main.js
@@ -14,22 +14,44 @@ do {
 document.getElementById("snak1Length").innerHTML = `Hai generato ${array1Sneck1.length} numeri, di cui la somma è ${x} Indi hai superato la tua quota di 50 e il giochino è finito. Buona giornata.`
 // !SECTION - Snecchino 1 fine
 
-// SECTION -Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
+// SECTION - Snecchino 2 - Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
 alert("Secondo snecchino")
 
 y =  parseInt (prompt ("inserisci il numero N numero"));
 
 do {
   z++;
-  // let array1snack2(z) = []
-  // console.log(array1snack2(z))
   let array1snack2 =[]
 
               for (i = 1; i <= 10; i++) {
                       x= Math.floor(Math.random() * 99 + 1);
-                      array1snack2.push(x);
-              }
-  document.getElementById("snecchino2_3_text").innerHTML  = "Hai selezionato " + y + " arrey"  
-  document.getElementById("snecchino2_3").innerHTML += `Il ${y}° arrey è <div>${array1snack2}</div>`;
+                      array1snack2.push(x);}
+
+  document.getElementById("snecchino2_3_text").innerHTML  = `Hai selezionato ${y} arrey`  
+  document.getElementById("snecchino2_3").innerHTML += `Il ${z}° arrey è <div>${array1snack2}</div>`;
 
 } while (z < y);
+
+// SECTION - Snecchino 3 - Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+alert("Terzo snecchino")
+
+z = parseInt(0)
+let snecchino3_x = parseInt(0);
+let snecchino3_y = parseInt(0);
+
+while (z < 5) {
+  z++
+    snecchino3_x = parseInt (prompt ("Inserisci un numero"));
+    snecchino3_y = snecchino3_x + snecchino3_y;
+    console.log(snecchino3_y);
+    document.getElementById("snecchino3").innerHTML = snecchino3_y;
+}
+// !SECTION
+
+// SECTION - Snecchino 4 (bonus) - In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa. Interrompi il ciclo appena il nome è stato trovato.
+
+alert ("Quarto snecchino (bonus)")
+z = parseInt(0)
+
+
