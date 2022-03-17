78c7ea75eb54d1a8159674a5b8d69aab5009cad8
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-17 19:10:20 +0000

    Snecchino 2 digerito

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var array1Sneck1 = [];
 var x = parseInt (0);
 var y = parseInt (0);
 var z = parseInt (0);
+
 do {
   let z = parseInt (prompt ("inserisci un numero"));
   x = x + z;
@@ -16,11 +17,19 @@ document.getElementById("snak1Length").innerHTML = `Hai generato ${array1Sneck1.
 // SECTION -Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
 alert("Secondo snecchino")
 
-
-y =  parseInt (prompt ("inserisci uil numero N numero"));
+y =  parseInt (prompt ("inserisci il numero N numero"));
 
 do {
-  z = z+1;
-  console.log(y)
-  console.log(z)
+  z++;
+  // let array1snack2(z) = []
+  // console.log(array1snack2(z))
+  let array1snack2 =[]
+
+              for (i = 1; i <= 10; i++) {
+                      x= Math.floor(Math.random() * 99 + 1);
+                      array1snack2.push(x);
+              }
+  document.getElementById("snecchino2_3_text").innerHTML  = "Hai selezionato " + y + " arrey"  
+  document.getElementById("snecchino2_3").innerHTML += `Il ${y}° arrey è <div>${array1snack2}</div>`;
+
 } while (z < y);
