4cef7a69fc91565c450a2754682d409dca145888
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-17 14:22:43 +0000

    1° Snecchino digerito

---
+++ main.js
@@ -0,0 +1,24 @@
+// SECTION - Snecchino 1 - Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+
+var array1Sneck1 = []
+var x = parseInt (0)
+var y = parseInt (0)
+
+// while (x => 50) {
+//   let z = prompt ("inserisci un numero")
+//   array1Sneck1.push(z)
+//   x += z
+// }
+
+// for ( i = 0; x <= 50; i++) {
+//   let z = prompt ("inserisci un numero");
+//   array1Sneck1.push(z);
+//   x += z;}
+
+do {
+  let z = parseInt (prompt ("inserisci un numero"));
+  x = x + z;
+  array1Sneck1.push(z);
+} while (x < 50);
+
+document.getElementById("snak1Length").innerHTML = `Hai generato ${array1Sneck1.length} numeri, di cui la somma è ${x} Indi hai superato la tua quota di 50 e il giochino è finito. Buona giornata.`
